a4967416a9c1dfb155feac8079e579a659990326
Joong Hyeon Kim <karis0222@gmail.com>
2016-12-26 18:50:18 +0900

    아이템용 모델과 마이그레이션 연계.

--- lists/tests.py
+++ lists/tests.py
@@ -4,6 +4,7 @@ from django.test import TestCase
 from django.core.urlresolvers import resolve
 import re
 
+from lists.models import Item
 from lists.views import home_page
 
 
@@ -21,7 +22,8 @@ class HomePageTest(TestCase):
     def test_home_page_returns_correct_html(self):
         request = HttpRequest()
         response = home_page(request)
-        expected_html = render_to_string('home.html')
+        expected_html = render_to_string('home.html')# Create your models here.
+
         self.assertEqual(self.remove_csrf(response.content.decode()), self.remove_csrf(expected_html))
 
     def test_home_page_can_save_a_POST_request(self):
@@ -36,3 +38,24 @@ class HomePageTest(TestCase):
         expected_html = render_to_string('home.html', {'new_item_text': '신규 작업 아이템'})
 
         self.assertEqual(self.remove_csrf(response.content.decode()), self.remove_csrf(expected_html))
+
+
+class ItemModelTest(TestCase):
+
+    def test_saving_and_retrieving_items(self):
+        first_item = Item()
+        first_item.text = '첫 번째 아이템'
+        first_item.save()
+
+        second_item = Item()
+        second_item.text = '두 번째 아이템'
+        second_item.save()
+
+        saved_items = Item.objects.all()
+        self.assertEqual(saved_items.count(), 2)
+
+        first_saved_item = saved_items[0]
+        second_saved_item = saved_items[1]
+        self.assertEqual(first_saved_item.text, '첫 번째 아이템')
+        self.assertEqual(second_saved_item.text, '두 번째 아이템')
+
