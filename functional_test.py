55682e7567e71a3f9e066b098afaabc6707c5d9a
Joong Hyeon Kim <karis0222@gmail.com>
2016-12-26 18:39:34 +0900

    작업목록 개수 추가에 대한 기능 테스트 확장 및 폼의 데이터 입력 후 POST 전송 기능 구현.

--- functional_test.py
+++ functional_test.py
@@ -12,6 +12,11 @@ class NewVisitorTest(unittest.TestCase):
     def tearDown(self):
         self.browser.quit()
 
+    def check_for_row_in_list_table(self, row_text):
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn(row_text, [row.text for row in rows])
+
     def test_can_start_a_list_and_retrieve_it_later(self):
         self.browser.get('http://127.0.0.1:8000')
 
@@ -23,12 +28,15 @@ class NewVisitorTest(unittest.TestCase):
         self.assertEqual(inputbox.get_attribute('placeholder'), '작업 아이템 입력')
 
         inputbox.send_keys('공작깃털 사기')
+        inputbox.send_keys(Keys.ENTER)
+        self.check_for_row_in_list_table('1: 공작깃털 사기')
 
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('공작깃털을 이용해서 그물 만들기')
         inputbox.send_keys(Keys.ENTER)
 
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-        self.assertTrue(any(row.text == '1: 공작깃털 사기' for row in rows), '신규 작업이 테이블에 표시되지 않는다.')
+        self.check_for_row_in_list_table('2: 공작깃털을 이용해서 그물 만들기')
+        self.check_for_row_in_list_table('1: 공작깃털 사기')
 
         self.fail('Finish the Test!')
 
--- lists/tests.py
+++ lists/tests.py
@@ -2,11 +2,17 @@ from django.http.request import HttpRequest
 from django.template.loader import render_to_string
 from django.test import TestCase
 from django.core.urlresolvers import resolve
+import re
 
 from lists.views import home_page
 
 
 class HomePageTest(TestCase):
+
+    @staticmethod
+    def remove_csrf(origin):
+        csrf_regex = r'<input[^>]+csrfmiddlewaretoken[^>]+>'
+        return re.sub(csrf_regex, '', origin)
 
     def test_root_url_resolves_to_home_page_view(self):
         found = resolve('/')
@@ -16,4 +22,17 @@ class HomePageTest(TestCase):
         request = HttpRequest()
         response = home_page(request)
         expected_html = render_to_string('home.html')
-        self.assertEqual(response.content.decode(), expected_html)
+        self.assertEqual(self.remove_csrf(response.content.decode()), self.remove_csrf(expected_html))
+
+    def test_home_page_can_save_a_POST_request(self):
+        request = HttpRequest()
+        request.method = 'POST'
+        request.POST['item_text'] = '신규 작업 아이템'
+
+        response = home_page(request)
+
+        self.assertIn('신규 작업 아이템', response.content.decode())
+
+        expected_html = render_to_string('home.html', {'new_item_text': '신규 작업 아이템'})
+
+        self.assertEqual(self.remove_csrf(response.content.decode()), self.remove_csrf(expected_html))
